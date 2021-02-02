319cdac6dc002080ab0bc5c528f978e8ed486d38
Unai Alberdi <ikbdr@plaiaundi.net>
2021-02-02 12:45:28 +0100

    first commit

---
+++ src/core/estadistica.java
@@ -0,0 +1,10 @@
+public class estadistica {
+    
+    public int valorDeUnTexto(String text) {
+        if int valor = 1 {
+            return 
+        }
+        
+        
+        return valor;
+}
---
+++ src/main/app.java
@@ -0,0 +1,10 @@
+package main;
+ import core.*;
+
+
+ public class app {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+ 
+}
